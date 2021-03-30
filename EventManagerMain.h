705fe1b8d827bfe1f91a4c715f09650de7ab8165
Kursat Kurkaya <kursatkurkaya@gmail.com>
2021-03-31 01:36:39 +0300

    EventManger Dosya Adı değiştirilip tekrar yüklendi.
    eski isimdeki dosyada nedense bozulma olayı tekrar tekrar yaşanıyor. Sıfırdan yeni bir dosya adı ile yeniden oluşturdum.

---
+++ EventManagerMain.h
@@ -0,0 +1,50 @@
+#include "EventCon.h"
+typedef void (*eventFunc);
+
+public: 
+  
+  EventCon events[40];
+  uint8_t currentCount = 0;
+  unsigned long debounceDelay = 50;
+  
+  EventManagerMain(){};
+  
+  void addEventListener(uint8_t argPinName, bool argEventType, bool argEventTarget, eventFunc argEventFunc){
+    events[currentCount] = EventCon(argPinName, argEventType, argEventTarget, argEventFunc);
+    pinMode(argPinName, INPUT_PULLUP);
+    currentCount++;  
+  }
+  
+  void loop(){
+    for(uint8_t i=0;i<currentCount;i++){
+      bool tmpRead = digitalRead(events[i].pName);
+
+    if(tmpRead != events[i].lState){
+        events[i].lState  = tmpRead;
+        events[i].lChange = millis();
+     }
+
+     if(millis() > events[i].lChange + debounceDelay){
+        events[i].lChange = millis();
+        events[i].state = tmpRead;
+     }
+
+     if(events[i].state == events[i].target ){
+
+          if(events[i].eType){
+            events[i].call();
+          }else{
+            if(!events[i].done){
+              events[i].done = true;
+              events[i].call();
+              
+            }
+          }
+     }else{
+       events[i].done = false;
+     }
+      
+    }
+  }
+
+};
--- ard.ino
+++ ard.ino
@@ -1,4 +1,4 @@
-#include "EventManager.h"
+#include "EventManagerMain.h"
 
 EventManager evMan;
 void setup() {
